dc0625e44a42a25d7b44b6500c715a9a3341eabe
Adam Casey <adam.jw.casey@gmail.com>
2023-01-11 21:10:55 -0700

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+/// Finds the top crates in each stack and returns a string representing these
+/// # Examples
+/// ```
+/// use std::fs;
+/// use advent_of_code_2022_05::find_top_crates;
+///
+/// let contents = fs::read_to_string("example-input.txt").unwrap();
+/// assert_eq!(find_top_crates(&contents), "CMZ");
+/// ```
+pub fn find_top_crates(input: &String) -> String {
+    todo!()
+}
---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use advent_of_code_2022_05::find_top_crates;
+use std::env;
+use std::fs;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let file_path = &args[1];
+
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+
+    let crates = find_top_crates(&contents);
+    println!("The top crates are: {crates}");
+}
