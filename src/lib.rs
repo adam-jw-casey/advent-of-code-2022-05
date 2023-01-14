1d96537b0719693ed2badc7baa89814791a5f0be
Adam Casey <adam.jw.casey@gmail.com>
2023-01-13 21:49:08 -0700

    Solves part two

--- src/lib.rs
+++ src/lib.rs
@@ -36,18 +36,30 @@ impl CrateStacks {
         let moved_crate = self.stacks[*from].pop().expect("Should not try to move from an empty stack.");
         self.stacks[*to].push(moved_crate);
     }
+
+    fn move_crates(&mut self, from: &usize, to: &usize, n: &usize){
+        let mut moved_stack = Vec::new();
+
+        for _i in 0..*n{
+            moved_stack.push(self.stacks[*from].pop().expect("Should not try to move from an empty stack"));
+        }
+        for _i in 0..*n{
+            self.stacks[*to].push(moved_stack.pop().unwrap());
+        }
+    }
 }
 
 /// Finds the top crates in each stack and returns a string representing these
+/// Uses the CrateMover 6000
 /// # Examples
 /// ```
 /// use std::fs;
-/// use advent_of_code_2022_05::find_top_crates;
+/// use advent_of_code_2022_05::find_top_crates_6000;
 ///
 /// let contents = fs::read_to_string("example-input.txt").unwrap();
-/// assert_eq!(find_top_crates(&contents), "CMZ");
+/// assert_eq!(find_top_crates_6000(&contents), "CMZ");
 /// ```
-pub fn find_top_crates(input: &String) -> String {
+pub fn find_top_crates_6000(input: &String) -> String {
     let (starting_stacks, raw_instructions) = input.split("\n\n").next_tuple().unwrap();
     let mut stacks = CrateStacks::new(starting_stacks);
     let steps: Vec<_> = raw_instructions.split('\n').filter(|x| !x.is_empty()).collect();
@@ -58,6 +70,30 @@ pub fn find_top_crates(input: &String) -> String {
         for _i in 0..n{
             stacks.move_crate(&(from-1), &(to-1)); // subtract 1 to 0-index
         }
+    } // Apply instructions
+
+    stacks.stacks.iter().map(|x| x.last().expect("Every stack should have at least one crate")).collect::<String>()
+}
+
+/// Finds the top crates in each stack and returns a string representing these
+/// Uses the CrateMover 6001
+/// # Examples
+/// ```
+/// use std::fs;
+/// use advent_of_code_2022_05::find_top_crates_6001;
+///
+/// let contents = fs::read_to_string("example-input.txt").unwrap();
+/// assert_eq!(find_top_crates_6001(&contents), "MCD");
+/// ```
+pub fn find_top_crates_6001(input: &String) -> String {
+    let (starting_stacks, raw_instructions) = input.split("\n\n").next_tuple().unwrap();
+    let mut stacks = CrateStacks::new(starting_stacks);
+    let steps: Vec<_> = raw_instructions.split('\n').filter(|x| !x.is_empty()).collect();
+
+    for step in steps{
+        let (n, from, to) = sscanf!(step, "move {} from {} to {}", usize, usize, usize).expect("input should be correctly formatted");
+
+        stacks.move_crates(&(from-1), &(to-1), &n);
     } // Apply instructions
 
     stacks.stacks.iter().map(|x| x.last().expect("Every stack should have at least one crate")).collect::<String>()
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use advent_of_code_2022_05::find_top_crates;
+use advent_of_code_2022_05::find_top_crates_6000;
+use advent_of_code_2022_05::find_top_crates_6001;
 use std::env;
 use std::fs;
 
@@ -8,6 +9,9 @@ fn main() {
 
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
 
-    let crates = find_top_crates(&contents);
-    println!("The top crates are: {crates}");
+    let mut crates = find_top_crates_6000(&contents);
+    println!("The top crates using CrateMover 6000 are: {crates}");
+
+    crates = find_top_crates_6001(&contents);
+    println!("The top crates using CrateMover 6001 are: {crates}");
 }
